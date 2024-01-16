afc789c1b5661d0be1a1bc17486f786aa6f103b1
Leaf, Andrew T <aleaf@usgs.gov>
2024-01-16 12:23:49 -0600

    ci(test_notebooks): add xfail list of Notebooks with intentional errors; ci(test.yaml): - bump scheduled ci to every day for now - update to use micromamba

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -1,9 +1,19 @@
 import os
+from pathlib import Path
 import shutil
 import glob
 import pytest
 
 
+# Notebooks that we don't expect to execute successfully
+# Notebook: reason
+xfail_notebooks = {
+    '00_python_basics_review.ipynb': 'intentional error',
+    '01_functions_scripts.ipynb': 'intentional error',
+    '00_python_basics_review__solutions.ipynb': 'intentional error',
+    '01_functions_script__solution.ipynb': 'intentional error',
+    
+}
 def included_notebooks():
     include = ['notebooks/part0_python_intro',
                'notebooks/part0_python_intro/solutions',
@@ -12,8 +22,16 @@ def included_notebooks():
                ]
     files = []
     for folder in include:
-        files += glob.glob(os.path.join(folder, '*.ipynb'))
-    return sorted(files)
+        files += sorted(list(Path(folder).glob('*.ipynb')))
+    files_with_xfails = []
+    for f in files:
+        if f.name in xfail_notebooks:
+            param_input = pytest.param(
+                f, marks=pytest.mark.xfail(reason=xfail_notebooks[f.name]))
+        else:
+            param_input = f
+        files_with_xfails.append(param_input)
+    return files_with_xfails
 
 
 @pytest.fixture(params=included_notebooks(), scope='module')
@@ -42,8 +60,8 @@ def test_notebook(notebook, testdir):
            '--ExecutePreprocessor.timeout=600 '
            '--ExecutePreprocessor.kernel_name=python3 '
            '--to ' + 'notebook '
-           '--execute ' + '{} '.format(notebook) +
-           '--output-dir ' + '{} '.format(testdir) +
-           '--output ' + '{}'.format(fname))
+           f'--execute {notebook} '
+           f'--output-dir {testdir} '
+           f'--output {fname}')
     ival = os.system(cmd)
-    assert ival == 0, 'could not run {}'.format(fname)
+    assert ival == 0, f'could not run {fname}'
