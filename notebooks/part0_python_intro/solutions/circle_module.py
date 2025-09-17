53f7e575adbe8fdf1275a89f35cc24d3ab5fae61
jlarsen-usgs <jlarsen@usgs.gov>
2025-09-17 15:45:28 -0700

    notebook updates for upcoming class

--- notebooks/part0_python_intro/solutions/circle_module.py
+++ notebooks/part0_python_intro/solutions/circle_module.py
@@ -20,3 +20,23 @@ class Circle(object):
 
     def __truediv__(self, other):
         return self.area / other.area
+
+
+
+class Pizza(Circle):
+    def __init__(self, x, ID, cost):
+        super().__init__(x, ID)
+        self.cost = cost
+
+    @property
+    def price_per_sq_inch(self):
+        return self.cost / self.area
+
+    def __floordiv__(self, other):
+        """
+        Using floor division to add another option for diving pizza price per sq inch.
+
+        This is because there really isn't another good option
+        """
+        return self.price_per_sq_inch / other.price_per_sq_inch
+
